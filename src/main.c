605e48bb361cf117b2fae3f8378dd1abb54ce0f0
Gunnar Þór Magnússon <gunnar.magnusson@booking.com>
2017-04-21 08:28:45 +0200

    More motd

--- src/main.c
+++ src/main.c
@@ -9,6 +9,10 @@ int main(int argc, char* argv[]) {
 
         char* motds[] = {
             "Why are you staring into outer space, crying?",
+            "You say you feel hollow, and you know it's because you are.",
+            "Here's what you get, for things that haven't happened yet.",
+            "See you later, if I see you at all.",
+            "Sunshine, been keeping me up for days.",
         };
 
         int i = rand() % ARR_SIZE(motds);
